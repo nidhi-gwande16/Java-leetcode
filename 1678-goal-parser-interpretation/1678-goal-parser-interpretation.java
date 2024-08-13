6f59e2cfd127561b8ca6411bef3c06d75f8c6e54
nidhi-gwande16 <133477564+nidhi-gwande16@users.noreply.github.com>
2024-08-13 19:03:49 +0530

    Time: 0 ms (100.00%), Space: 40.9 MB (99.52%) - LeetHub

--- 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
+++ 1678-goal-parser-interpretation/1678-goal-parser-interpretation.java
@@ -1,8 +1,29 @@
 class Solution {
     public String interpret(String command) {
-      String s=command.replace("()","o");
-        s=s.replace("(al)","al");
+      StringBuilder sb=new StringBuilder();
+        int i=0;
+        int n=command.length();
+        while(i<n)
+        {
+            char ch=command.charAt(i);
+            if(ch=='G')
+                sb.append('G');
+            else if(ch=='(')
+            {
+                if(command.charAt(i+1)==')')
+                {
+                    sb.append("o");
+                    i++;
+                        
+                }
+                else{
+                    sb.append("al");
+                    i=i+3;
+                }
+            }
+            i++;
+        }
+        return sb.toString();
         
-        return s;
     }
 }
