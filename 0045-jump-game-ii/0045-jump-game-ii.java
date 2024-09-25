837bfb0d73582f331969e72b97408b7fc9e36772
nidhi-gwande16 <133477564+nidhi-gwande16@users.noreply.github.com>
2024-09-25 18:05:59 +0530

    Time: 1 ms (98.13%), Space: 45.2 MB (34.57%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int jump(int[] nums) {
+        if(nums.length<=2) 
+            return nums.length-1;
+        int max=nums[0] ;
+        int count=1 , end=nums[0];
+        
+        for(int i=1; i<nums.length-1;i++)
+        {
+            max=Math.max(max,nums[i]+i);
+            if(i==end)
+            {
+                end=max;
+                count++;
+            }
+        }
+        return count;
+    }
+}
